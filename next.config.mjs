ff9a67dd2274452e4b31604aa9e3ad6626688486
jose32coder <josemanuel180601@gmail.com>
2026-04-20 21:43:22 -0400

    Despliegue en Cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   // Permitir acceso desde móviles/IPs locales en desarrollo para HMR
   allowedDevOrigins: ["192.168.0.203", "10.2.0.2", "localhost:3000"],
-  
+
   // Solucionar conflicto de Turbopack (Next.js 15/16)
   turbopack: {},
 
